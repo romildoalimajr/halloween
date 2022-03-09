9e6170d7d5f5a89fffaae97ca0998b5215c3248c
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-03-09 06:52:39 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
 };
+//scroll spy
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.header .navbar a');
 
 window.onscroll = () => {
     searchForm.classList.remove('active');
@@ -21,6 +24,19 @@ window.onscroll = () => {
     } else {
         document.querySelector('.header').classList.remove('active');
     }
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.header .navbar a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    });
 };
 
 window.onload = () => {
@@ -43,5 +59,27 @@ var swiper = new Swiper(".home-slider", {
     autoplay: {
         delay: 9500,
         disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".products-slider", {
+    spaceBetween: 20,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    grabCursor: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
     },
 });
